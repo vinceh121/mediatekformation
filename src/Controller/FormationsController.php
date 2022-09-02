e1cfab255a7c7dd01f468cb0138e8e2d66ce64c5
CNED-SLAM <emds@bodhissatva.com>
2022-09-02 18:31:58 +0200

    Phase 5 : Gestion des tris et filtres. Task #4 - Phase 5 : Gestion des tris et filtres.

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +19,17 @@ class FormationsController extends AbstractController {
      * 
      * @var FormationRepository
      */
-    private $repository;
+    private $formationRepository;
     
-    function __construct(FormationRepository $repository) {
-        $this->repository = $repository;
+    /**
+     * 
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
     }
     
     /**
@@ -28,10 +37,47 @@ class FormationsController extends AbstractController {
      * @return Response
      */
     public function index(): Response{
-        $formations = $this->repository->findAll();
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
         return $this->render("pages/formations.html.twig", [
-            'formations' => $formations
+            'formations' => $formations,
+            'categories' => $categories
         ]);
     }
+
+    /**
+     * @Route("/formations/tri/{champ}/{ordre}/{table}", name="formations.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @param type $table
+     * @return Response
+     */
+    public function sort($champ, $ordre, $table=""): Response{
+        $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render("pages/formations.html.twig", [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }     
+    
+    /**
+     * @Route("/formations/recherche/{champ}/{table}", name="formations.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @param type $table
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request, $table=""): Response{
+        $valeur = $request->get("recherche");
+        $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
+        $categories = $this->categorieRepository->findAll();
+        return $this->render("pages/formations.html.twig", [
+            'formations' => $formations,
+            'categories' => $categories,
+            'valeur' => $valeur,
+            'table' => $table
+        ]);
+    }  
     
 }
--- src/Repository/FormationRepository.php
+++ src/Repository/FormationRepository.php
@@ -39,4 +39,56 @@ class FormationRepository extends ServiceEntityRepository
         }
     }
 
+    /**
+     * Retourne toutes les formations triées sur un champ
+     * @param type $champ
+     * @param type $ordre
+     * @param type $table si $champ dans une autre table
+     * @return Formation[]
+     */
+    public function findAllOrderBy($champ, $ordre, $table=""): array{
+        if($table==""){
+            return $this->createQueryBuilder('f')
+                    ->orderBy('f.'.$champ, $ordre)
+                    ->getQuery()
+                    ->getResult();
+        }else{
+            return $this->createQueryBuilder('f')
+                    ->join('f.'.$table, 't')
+                    ->orderBy('t.'.$champ, $ordre)
+                    ->getQuery()
+                    ->getResult();            
+        }
+    }
+
+    /**
+     * Enregistrements dont un champ contient une valeur
+     * ou tous les enregistrements si la valeur est vide
+     * @param type $champ
+     * @param type $valeur
+     * @param type $table si $champ dans une autre table
+     * @return Formation[]
+     */
+    public function findByContainValue($champ, $valeur, $table=""): array{
+        if($valeur==""){
+            return $this->findAll();
+        }
+        if($table==""){
+            return $this->createQueryBuilder('f')
+                    ->where('f.'.$champ.' LIKE :valeur')
+                    ->orderBy('f.publishedAt', 'DESC')
+                    ->setParameter('valeur', '%'.$valeur.'%')
+                    ->getQuery()
+                    ->getResult();            
+        }else{
+            return $this->createQueryBuilder('f')
+                    ->join('f.'.$table, 't')                    
+                    ->where('t.'.$champ.' LIKE :valeur')
+                    ->orderBy('f.publishedAt', 'DESC')
+                    ->setParameter('valeur', '%'.$valeur.'%')
+                    ->getQuery()
+                    ->getResult();                   
+        }       
+    }    
+    
 }
