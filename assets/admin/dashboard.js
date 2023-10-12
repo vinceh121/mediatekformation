f0c7f14470ca54344500268e236bb2c83464c225
vinceh121 <contact@vinceh121.me>
2023-10-12 07:42:44 +0200

    feat: formation categories in table (#2)

--- assets/admin/dashboard.js
+++ assets/admin/dashboard.js
@@ -6,18 +6,18 @@ import language from 'datatables.net-plugins/i18n/fr-FR';
 $(document).ready(() => {
 	const tblFormations = new DataTable('#tblFormations', {
 		language: language,
-		order: [[2, 'desc']],
+		order: [[3, 'desc']],
 		columnDefs: [
-			{
-				target: 3,
-				orderable: false
-			},
 			{
 				target: 4,
 				orderable: false
 			},
 			{
 				target: 5,
+				orderable: false
+			},
+			{
+				target: 6,
 				orderable: false
 			}
 		]
