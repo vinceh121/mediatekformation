666d8120a45aee4f797967d851092da9cb42d6a1
vinceh121 <contact@vinceh121.me>
2023-10-12 05:26:37 +0200

    feat: formation edit

---
+++ src/Controller/Admin/AdminFormationController.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Form\FormationType;
+use App\Repository\FormationRepository;
+use App\Repository\CategorieRepository;
+use App\Repository\PlaylistRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ *
+ * @Route("/admin/formation", name="admin_formation_")
+ */
+class AdminFormationController extends AbstractController
+{
+
+    private FormationRepository $formationRepository;
+
+    private CategorieRepository $categoryRepository;
+
+    private PlaylistRepository $playlistsRepository;
+
+    private EntityManagerInterface $em;
+
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categoryRepository, PlaylistRepository $playlistsRepository, EntityManagerInterface $em)
+    {
+        $this->formationRepository = $formationRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->playlistsRepository = $playlistsRepository;
+        $this->em = $em;
+    }
+
+    /**
+     *
+     * @Route("/{formationId}", name="update")
+     */
+    public function update(Request $request, int $formationId): Response
+    {
+        $formation = $this->formationRepository->find($formationId);
+
+        if (!$formation) {
+            throw $this->createNotFoundException('Formation inconnue');
+        }
+
+        $categories = $this->categoryRepository->findAll();
+        $playlists = $this->playlistsRepository->findAll();
+
+        $form = $this->createForm(FormationType::class, $formation, [
+            'categories' => $categories,
+            'playlists' => $playlists
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($form->getData());
+            $this->em->flush();
+
+            $this->addFlash('success', 'Changements enregistrés');
+        }
+
+        return $this->render('admin/formation.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
+
---
+++ src/Form/FormationType.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Categorie;
+use App\Entity\Playlist;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FormationType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('publishedAt', DateTimeType::class)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('videoId', TextType::class)
+            ->add('playlist', EntityType::class, [
+            'class' => Playlist::class,
+            'choices' => $options['playlists'],
+            'choice_label' => function (Playlist $playlist): string {
+                return $playlist->getName();
+            }
+        ])
+            ->add('categories', EntityType::class, [
+            'class' => Categorie::class,
+            'choices' => $options['categories'],
+            'choice_label' => function (Categorie $category): string {
+                return $category->getName();
+            },
+            'expanded' => true,
+            'multiple' => true
+        ])
+            ->add('submit', SubmitType::class, [
+            'label' => 'Connexion'
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+            'playlists' => [],
+            'categories' => []
+        ]);
+    }
+}
