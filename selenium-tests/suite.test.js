5581b7f0f10d2b35d021dd665293ff91c8fe4c56
vinceh121 <contact@vinceh121.me>
2024-01-05 22:52:40 +0100

    feat: integration tests with Selenium

---
+++ selenium-tests/suite.test.js
@@ -0,0 +1,41 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const firefox = require("selenium-webdriver/firefox");
+
+const { BROWSER, BASE_URL } = process.env;
+
+/**
+ * @type {import("selenium-webdriver").ThenableWebDriver}
+ */
+let driver;
+beforeAll(async () => {
+	driver = await new Builder().forBrowser(BROWSER)
+		.setChromeOptions(new chrome.Options()
+			.addArguments("--headless")
+			.addArguments("--window-size=1920,1080"))
+		.setFirefoxOptions(new firefox.Options()
+			.addArguments("--headless"))
+		.build();
+}, 10_000);
+
+it("should navigate to home", async () => {
+	await driver.get(BASE_URL + "/");
+	expect(await driver.getTitle()).toBe(""); // TODO set a title
+});
+
+it("should navigate to formations using navbar", async () => {
+	await driver.get(BASE_URL + "/");
+	const anchor = await driver.findElement(By.css('.nav-link[href="/formations"]'));
+	await anchor.click();
+	expect(await driver.getCurrentUrl()).toContain("/formations");
+	expect(await driver.getTitle()).toBe(""); // TODO set a title
+});
+
+it("should sort formations by title", async () => {
+	await driver.get(BASE_URL + "/formations");
+	await (await driver.findElement(By.css('a[href="/formations/tri/title/DESC"]'))).click();
+	expect(await driver.getCurrentUrl()).toContain("/formations/tri/title/DESC");
+
+	const firstFormation = await driver.findElement(By.css("tbody tr h5"));
+	expect(await firstFormation.getText()).toBe("title 3");
+});
